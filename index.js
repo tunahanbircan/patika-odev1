47d6e654559f4d98cd42a51d6ef37a94c7cfc15e
Tunahan Bircan <tunahanbircan42@gmail.com>
2021-05-14 19:45:32 +0300

    Implement clock feature

---
+++ index.js
@@ -0,0 +1,40 @@
+function getName() {
+    const name = prompt('Adınız nedir?');
+    document.getElementById('myName').innerText = name;
+  }
+  
+  function getTime(date) {
+    const h = date.getHours();
+    const m = date.getMinutes();
+    const s = date.getSeconds();
+  
+    return {
+      h: h < 10 ? `0 ${h}` : h,
+      m: m < 10 ? `0 ${m}` : m,
+      s: s < 10 ? `0 ${s}` : s,
+    }
+  }
+  
+  function showDateTime(currentTime, currentDay) {
+    return `${currentTime.h}:${currentTime.m}:${currentTime.s} ${currentDay}`;
+  };
+  
+  function getDateTime() {
+   const date = new Date();
+   let currentDay = date.getDay();
+  
+   const week = ['Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi', 'Pazar'];
+  
+   currentDay = week[currentDay - 1];
+   const currentTime = getTime(date);
+  
+   const dateTime = showDateTime(currentTime, currentDay);
+  
+   document.getElementById('myClock').innerText = dateTime;
+   document.getElementById('myClock').textContent = dateTime;
+  
+   setTimeout(getDateTime, 1000);
+  }
+  
+  getName();
+  getDateTime();
