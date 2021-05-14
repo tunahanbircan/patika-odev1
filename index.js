dbb678322e35428c74f18593abffac7792065833
Tunahan Bircan <tunahanbircan42@gmail.com>
2021-05-14 19:51:06 +0300

    Remove spacing

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ function getName() {
     const s = date.getSeconds();
   
     return {
-      h: h < 10 ? `0 ${h}` : h,
-      m: m < 10 ? `0 ${m}` : m,
-      s: s < 10 ? `0 ${s}` : s,
+      h: h < 10 ? `0${h}` : h,
+      m: m < 10 ? `0${m}` : m,
+      s: s < 10 ? `0${s}` : s,
     }
   }
   
